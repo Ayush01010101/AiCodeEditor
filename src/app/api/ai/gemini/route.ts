fb6abed52b1758106f7be5956d9b669e25001bbb
Ayush01010101 <ayushawachar0989@gmail.com>
2026-02-12 17:36:52 +0530

    feat(api-ai): add api endpoint for google gemini - adds `/api/ai/gemini` route for text generation - uses the ai SDK to connect with google's models - integrates the `gemini-3-flash-preview` model - pulls in new packages: `@ai-sdk/google` and `ai`

---
+++ src/app/api/ai/gemini/route.ts
@@ -0,0 +1,13 @@
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+import { NextResponse } from 'next/server';
+
+
+export async function POST(request: Request, responce: Response) {
+  const { text } = await generateText({
+    model: google('gemini-3-flash-preview'),
+    prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+  });
+  return NextResponse.json({ text })
+}
+
