af26c7c5a1529de7b860cbb41d2741dca661b11c
Ayush01010101 <ayushawachar0989@gmail.com>
2026-02-12 15:37:04 +0530

    feat(ui): introduce new item component
    - adds a versatile `Item` component to `components/ui`
    - comes with `ItemMedia`, `ItemContent`, `ItemTitle`, `ItemDescription`, and `ItemActions` for building structured content
    - uses the new item component in `UnauthorizedView` for a cleaner look
    - removes the old basic unauthorized message

--- src/app/Features/Auth/UnAuthorizedView.tsx
+++ src/app/Features/Auth/UnAuthorizedView.tsx
@@ -1,11 +1,27 @@
 "use client"
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+} from "@/components/ui/item"
 const UnauthorizedView = () => {
 
   return (
-    <div className="text-4xl text-white">
+    <div className=" w-screen flex items-center p-5 justify-center   flex-col gap-6">
+      <Item variant="outline" >
+        <ItemContent >
+          <ItemTitle className="text-3xl font-medium">Unauthorized</ItemTitle>
+          <ItemDescription className="text-2xl">
+            You are not authorized to view this page
+          </ItemDescription>
+        </ItemContent>
+        <ItemActions>
+        </ItemActions>
+      </Item>
 
-      You are unauthorized please login to continue
-    </div>
+    </div >
   )
 }
 export default UnauthorizedView
